902c46af7de386f9dbc46c4b2194f5d4ae4089a1
Istiaq Fuad <istiaqfuad123@gmail.com>
2022-01-17 01:42:46 +0600

    commit before making documentation

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use("/api/v1/jobs", auth, jobRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -25,16 +25,37 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ job });
 };
 
+// const updateJob = async (req, res) => {
+//   const {
+//     body: { company, position },
+//     user: { userId },
+//     params: { id: jobId },
+//   } = req;
+//   if (company === "" || position === "") {
+//     throw new BadRequestError("company or position field can not be empty");
+//   }
+//   const job = Job.findOneAndUpdate(
+//     { _id: jobId, createdBy: userId },
+//     req.body,
+//     { new: true, runValidators: true }
+//   );
+//   if (!job) {
+//     throw new NotFoundError(`No job with id ${jobId}`);
+//   }
+//   res.status(StatusCodes.OK).json({ job });
+// };
+
 const updateJob = async (req, res) => {
   const {
     body: { company, position },
     user: { userId },
     params: { id: jobId },
   } = req;
+
   if (company === "" || position === "") {
-    throw new BadRequestError("company or position field can not be empty");
+    throw new BadRequestError("Company or Position fields cannot be empty");
   }
-  const job = Job.findOneAndUpdate(
+  const job = await Job.findByIdAndUpdate(
     { _id: jobId, createdBy: userId },
     req.body,
     { new: true, runValidators: true }
